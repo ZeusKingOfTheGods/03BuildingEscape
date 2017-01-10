3ed6bc57bdef287aebde2a562c005b8f1336e917
SINTLUCAS\j.vulcano <jvjader@gmail.com>
2017-01-10 15:44:11 +0100

    Moving objects in C++

---
+++ 03BuildingEscape/BuidlingEscape/Source/BuidlingEscape/OpenDoor.cpp
@@ -0,0 +1,39 @@
+// Copyright Jader Vulcano
+
+#include "BuidlingEscape.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto* Owner = GetOwner();
+
+	FRotator NewRotation = FRotator(0.f, 90.f, 0.f);
+
+	Owner->SetActorRotation(NewRotation);
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
---
+++ 03BuildingEscape/BuidlingEscape/Source/BuidlingEscape/OpenDoor.h
@@ -0,0 +1,24 @@
+// Copyright Jader Vulcano
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "OpenDoor.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUIDLINGESCAPE_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+	
+};
